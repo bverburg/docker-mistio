8c9301792bf8f592cfef29333618f093ffbfcf24
roli keusch <mail@keus.ch>
2014-04-01 17:03:14 +0200

    First Commit

---
+++ mistio-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec sh -c "/opt/mistio/bin/paster serve /opt/mistio/production.ini --reload"
---
+++ settings.py
@@ -0,0 +1,25 @@
+"""User defined settings"""
+
+## URI of hosted mist service
+#CORE_URI = "https://mist.io"
+
+## Verify SSL certificates when communicating with core service
+#SSL_VERIFY = True
+
+## For Better Performance use this to settings to true
+## Use minified (uglified) js
+JS_BUILD = True 
+
+## Use minified css 
+CSS_BUILD = True
+
+## Javascript logging verbosity level
+#JS_LOG_LEVEL = 3
+
+
+## Google analytics ID
+#GOOGLE_ANALYTICS_ID = ""
+
+
+## SSH connection timeout
+#COMMAND_TIMEOUT = 20
